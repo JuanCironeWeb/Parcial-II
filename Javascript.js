f55b6df9a7602a46c32677e0b9a8a9f825df51f7
JuanCironeWeb <132408737+JuanCironeWeb@users.noreply.github.com>
2023-06-20 20:50:58 -0300

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,12 @@
+'use strict'
+
+
+let productos = []; 
+
+productos["imagen"] = []; 
+productos["nombre"] = []; 
+productos["descripcion"] = []; 
+productos["precio"] = []; 
+productos["categoria"] = [];
+
+
